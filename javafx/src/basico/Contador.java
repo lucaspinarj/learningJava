e0a87ad293812a6d25ae4291c3e7cf7f835821c4
lucaspinarj <lucaspinarj@hotmail.com>
2022-09-29 09:56:54 -0300

    personalizando2

--- javafx/src/basico/Contador.java
+++ javafx/src/basico/Contador.java
@@ -12,6 +12,19 @@ import javafx.stage.Stage;
 public class Contador extends Application{
 	
 	private int contador = 0;
+	
+	private void atualizarLabelNumero(Label label) {
+		label.setText(Integer.toString(contador));
+		
+		label.getStyleClass().remove("verde");
+		label.getStyleClass().remove("vermelho");
+		
+		if (contador > 0) {
+			label.getStyleClass().add("verde");
+		} else if (contador < 0){
+			label.getStyleClass().add("vermelho");
+		}
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -23,15 +36,18 @@ public class Contador extends Application{
 		labelNumero.getStyleClass().add("numero");
 		
 		Button botaoIncremento = new Button("+");
+		botaoIncremento.getStyleClass().add("botoes");
 		botaoIncremento.setOnAction(e -> {
 			contador++;
 			labelNumero.setText(Integer.toString(contador));
+			atualizarLabelNumero(labelNumero);
 		});
 		
 		Button botaoDecremento = new Button("-");
+		botaoDecremento.getStyleClass().add("botoes");
 		botaoDecremento.setOnAction(e -> {
 			contador --;
-			labelNumero.setText(Integer.toString(contador));
+			atualizarLabelNumero(labelNumero);
 		});
 		
 		HBox boxBotoes = new HBox();
